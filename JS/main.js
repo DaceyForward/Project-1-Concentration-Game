de23efc6f144c75361ae70146dd5a6eb6c0d7356
daceyforward <dacey.loving@gmail.com>
2023-08-04 17:01:11 -0400

    pseudocode in js

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,54 @@
 /*----- constants -----*/
+// these are elements that will NOT change through the course of the game
+// ?? could I somehow add the background image in here..
+// back color of game tiles (grey), matching pairs, 
+// images: beachBall, snorkel, sun, palmTree, floats, sunscreen, sunglasses, sandCastle, sandals, popsicle, watermelon, pineapple, surfboard, swimsuit, umbrella
 
+const backOfTiles = {
+    0: 'grey'
+}
+
+const matchingPairs = [
+    {name: 'beachBall', image: 'tile images/image.png'},
+    {name: 'snorkel', image: 'tile images/image (1).png'},
+    {name: 'sun', image: 'tile images/image (2).png'},
+    {name: 'palmTree', image: 'tile images/image (3).png'},
+    {name: 'floats', image: 'tile images/image (4).png'},
+    {name: 'sunscreen', image: 'tile images/image (5).png'},
+    {name: 'sunglasses', image: 'tile images/image (6).png'},
+    {name: 'sandCastle', image: 'tile images/image (7).png'},
+    {name: 'sandals', image: 'tile images/image (8).png'},
+    {name: 'popsicle', image: 'tile images/image (9).png'},
+    {name: 'watermelon', image: 'tile images/image (10).png'},
+    {name: 'pineapple', image: 'tile images/image (11).png'},
+    {name: 'surfboard', image: 'tile images/image (12).png'},
+    {name: 'swimsuit', image: 'tile images/image (13).png'},
+    {name: 'umbrella', image: 'tile images/image (14).png'}
+]
 
 /*----- state variables -----*/
+// these are elements that WILL change through the course of the game
+// score, timer, images of the tiles, location of the tiles (shuffle)
+
 
 
 /*----- cached elements  -----*/
+// save necessary html elements as variables so we can refer to them later
+// button, message, tiles, score, timer
 
-
-/*----- event listeners -----*/
 
 
 /*----- functions -----*/
+// make things start to happen in the background with functions
+// initialize the game by setting values for variables
+// renders functions - board, scores, button, timer
+// render function
+// shuffle function
+// checking for a matching pair of tiles
+// handleClick function
+
+
+
+/*----- event listeners -----*/
+// what events happen when buttons and tiles are clicked, what they should be attached to, and what functions they call
+// handleClick and button
