d1d408da7230bda9fb95840e9741801fb997f00a
daceyforward <dacey.loving@gmail.com>
2023-08-06 21:33:23 -0400

    add tile images and updates to html, css, js

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,13 @@
 /*----- constants -----*/
 // these are elements that will NOT change through the course of the game
-// ?? could I somehow add the background image in here..
-// back color of game tiles (grey), matching pairs, 
+// back color of game tiles (grey), matching pairs...?
 // images: beachBall, snorkel, sun, palmTree, floats, sunscreen, sunglasses, sandCastle, sandals, popsicle, watermelon, pineapple, surfboard, swimsuit, umbrella
 
 const backOfTiles = {
     0: 'grey'
 }
 
-const matchingPairs = [
+const tileImages = [
     {name: 'beachBall', image: 'tile images/image.png'},
     {name: 'snorkel', image: 'tile images/image (1).png'},
     {name: 'sun', image: 'tile images/image (2).png'},
@@ -30,13 +29,36 @@ const matchingPairs = [
 // these are elements that WILL change through the course of the game
 // score, timer, images of the tiles, location of the tiles (shuffle)
 
+let score
+// begins at zero and increases with each matching pair the player chooses
+// only increases with matching pairs, not on every click
+// max number shown will be 15
+// when the score reaches 15, display winning message
+
+let timer
+// starting from zero, when the first tile is clicked it begins timing the game play
+// continuously used through game
+// stops when the last pair of matching tiles is chosen
+
+let tiles
+// location of tiles shuffles after every game, not on every turn
+// have an image associated with a tile number or something? 
 
 
 /*----- cached elements  -----*/
 // save necessary html elements as variables so we can refer to them later
 // button, message, tiles, score, timer
 
-
+const messageEl = document.querySelector('#message')
+//console.log('this is the message element', messageEl)
+const scoreEl = document.querySelector('#score')
+//console.log('this is the score element', scoreEl)
+const timerEl = document.querySelector('#timer')
+//console.log('this is the timer element', timerEl)
+const playAgainButton = document.querySelector('button')
+//console.log('this is the button element', playAgainButton)
+const tileEls = [...document.querySelectorAll('.tiles > div')]
+//onsole.log('this is the tile element', tileEls)
 
 /*----- functions -----*/
 // make things start to happen in the background with functions
@@ -46,6 +68,53 @@ const matchingPairs = [
 // shuffle function
 // checking for a matching pair of tiles
 // handleClick function
+
+init()
+
+function init() {
+    score = 0
+    timer = 0
+    tiles = [
+        [0, 0, 0, 0, 0], // col 0
+        [0, 0, 0, 0, 0], // col 1
+        [0, 0, 0, 0, 0], // col 2
+        [0, 0, 0, 0, 0], // col 3
+        [0, 0, 0, 0, 0], // col 4
+        [0, 0, 0, 0, 0] // col 5
+    ]
+    render()
+}
+
+function renderScore() {
+
+}
+
+function renderTimer() {
+
+}
+
+function renderTiles() {
+
+}
+
+function renderButton() {
+    playAgainButton.style.visibility = winner ? 'visible' : 'hidden'
+}
+
+function render(){
+    renderScore()
+    renderTimer()
+    renderTiles()
+    renderButton()
+}
+
+function shuffleTiles() {
+
+}
+
+function handleClick() {
+
+}
 
 
 
