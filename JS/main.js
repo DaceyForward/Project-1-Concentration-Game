08f6c8958f512cc90132c862de0e3c4fdefbc78c
daceyforward <dacey.loving@gmail.com>
2023-08-07 12:14:56 -0400

    javascript functions and updating constants

--- JS/main.js
+++ JS/main.js
@@ -6,23 +6,38 @@
 const backOfTiles = {
     0: 'grey'
 }
-
+// should the match be labeled "beachBall2" or the same as the name?
 const tileImages = [
-    {name: 'beachBall', image: 'tile images/image.png'},
-    {name: 'snorkel', image: 'tile images/image (1).png'},
-    {name: 'sun', image: 'tile images/image (2).png'},
-    {name: 'palmTree', image: 'tile images/image (3).png'},
-    {name: 'floats', image: 'tile images/image (4).png'},
-    {name: 'sunscreen', image: 'tile images/image (5).png'},
-    {name: 'sunglasses', image: 'tile images/image (6).png'},
-    {name: 'sandCastle', image: 'tile images/image (7).png'},
-    {name: 'sandals', image: 'tile images/image (8).png'},
-    {name: 'popsicle', image: 'tile images/image (9).png'},
-    {name: 'watermelon', image: 'tile images/image (10).png'},
-    {name: 'pineapple', image: 'tile images/image (11).png'},
-    {name: 'surfboard', image: 'tile images/image (12).png'},
-    {name: 'swimsuit', image: 'tile images/image (13).png'},
-    {name: 'umbrella', image: 'tile images/image (14).png'}
+    {name: 'beachBall1', image: 'tile images/image.png', match: 'beachBall2', orderNum: 1},
+    {name: 'snorkel1', image: 'tile images/image (1).png', match: 'snorkel2', orderNum: 2},
+    {name: 'sun1', image: 'tile images/image (2).png', match: 'sun2', orderNum: 3},
+    {name: 'palmTree1', image: 'tile images/image (3).png', match: 'palmTree2', orderNum: 4},
+    {name: 'floats1', image: 'tile images/image (4).png', match: 'floats2', orderNum: 5},
+    {name: 'sunscreen1', image: 'tile images/image (5).png', match: 'sunscreen2', orderNum: 6},
+    {name: 'sunglasses1', image: 'tile images/image (6).png', match: 'sunglasses2', orderNum: 7},
+    {name: 'sandCastle1', image: 'tile images/image (7).png', match: 'sandCastle2', orderNum: 8},
+    {name: 'sandals1', image: 'tile images/image (8).png', match: 'sandals2', orderNum: 9},
+    {name: 'popsicle1', image: 'tile images/image (9).png', match: 'popsicle2', orderNum: 10},
+    {name: 'watermelon1', image: 'tile images/image (10).png', match: 'watermelon2', orderNum: 11},
+    {name: 'pineapple1', image: 'tile images/image (11).png', match: 'pineapple2', orderNum: 12},
+    {name: 'surfboard1', image: 'tile images/image (12).png', match: 'surfboard2', orderNum: 13},
+    {name: 'swimsuit1', image: 'tile images/image (13).png', match: 'swimsuit2', orderNum: 14},
+    {name: 'umbrella1', image: 'tile images/image (14).png', match: 'umbrella2', orderNum: 15},
+    {name: 'beachBall2', image: 'tile images/image.png', match: 'beachBall1', orderNum: 16},
+    {name: 'snorkel2', image: 'tile images/image (1).png', match: 'snorkel1', orderNum: 17},
+    {name: 'sun2', image: 'tile images/image (2).png', match: 'sun1', orderNum: 18},
+    {name: 'palmTree2', image: 'tile images/image (3).png', match: 'palmTree1', orderNum: 19},
+    {name: 'floats2', image: 'tile images/image (4).png', match: 'floats1', orderNum: 20},
+    {name: 'sunscreen2', image: 'tile images/image (5).png', match: 'sunscreen1', orderNum: 21},
+    {name: 'sunglasses2', image: 'tile images/image (6).png', match: 'sunglasses1', orderNum: 22},
+    {name: 'sandCastle2', image: 'tile images/image (7).png', match: 'sandCastle1', orderNum: 23},
+    {name: 'sandals2', image: 'tile images/image (8).png', match: 'sandals1', orderNum: 24},
+    {name: 'popsicle2', image: 'tile images/image (9).png', match: 'popsicle1', orderNum: 25},
+    {name: 'watermelon2', image: 'tile images/image (10).png', match: 'watermelon1', orderNum: 26},
+    {name: 'pineapple2', image: 'tile images/image (11).png', match: 'pineapple1', orderNum: 27},
+    {name: 'surfboard2', image: 'tile images/image (12).png', match: 'surfboard1', orderNum: 28},
+    {name: 'swimsuit2', image: 'tile images/image (13).png', match: 'swimsuit1', orderNum: 29},
+    {name: 'umbrella2', image: 'tile images/image (14).png', match: 'umbrella1', orderNum: 30}
 ]
 
 // const player = 'null'
@@ -84,7 +99,7 @@ function init() {
         [0, 0, 0, 0, 0], // col 2
         [0, 0, 0, 0, 0], // col 3
         [0, 0, 0, 0, 0], // col 4
-        [0, 0, 0, 0, 0] // col 5
+        [0, 0, 0, 0, 0]  // col 5
     ]
     render()
 }
@@ -93,7 +108,9 @@ function init() {
 // want to start at 0 and increase by 1 with each matching pair of images clicked
 // will not exceed 15
 function renderScore() {
-
+    for (let i = 0; i === match; 1++) {
+        return score += 1
+    } if 
 }
 
 // displays timer on page
@@ -112,7 +129,7 @@ function renderTimer() {
     timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`
 }
 
-// displays all tiles face down until clickec
+// displays all tiles face down until clicked
 // want tile to flip over to display the image when clicked, and stay on this side when another tile is clicked
 // if the two tiles are a match, tiles stay face up
 // if the two tiles are NOT a match, flip back to face down after 1 second
@@ -133,11 +150,11 @@ function renderButton() {
 // if tiles clicked are not a match, display "Not a match. Try again!"
 // if all 15 matching pairs are clicked, display "YOU WIN!!!"
 function renderMessage() {
-    if () {
+    if (score += 1) {
         messageEl.innerText = "You got a match!!"
-    } else if () {
+    } else if (score) {
         messageEl.innerText = "Not a match. Try Again!"
-    } else (score === 15) {
+    } else if (score === 15) {
         messageEl.innerText = "YOU WIN!!!"
     }
 }
@@ -154,9 +171,23 @@ function render() {
 // moves tiles around when play again button is clicked
 // (do I need to render this?)
 // want tiles to move to a new and random position when the play again button is clicked
+// use fisher-yates method??
 function shuffleTiles() {
-    
+
 }
+
+// const points = [40, 100, 1, 5, 25, 10];
+// document.getElementById("demo").innerHTML = points;  
+
+// function shuffleTiles() {
+//   for (let i = points.length -1; i > 0; i--) {
+//     let j = Math.floor(Math.random() * (i+1));
+//     let k = points[i];
+//     points[i] = points[j];
+//     points[j] = k;
+//   }
+//   document.getElementById("demo").innerHTML = points;
+// }
 
 // when the player clicks on tiles or button
 // want tiles to flip over when clicked
