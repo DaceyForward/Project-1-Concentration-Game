b8352e3695ea3a6044f9a9bbd6b38b38dbc0c2c4
daceyforward <dacey.loving@gmail.com>
2023-08-11 00:35:02 -0400

    updates to MVP section on readme

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,8 @@
-// constants ------------------------------------
+// constants -------------------------------------------------------------------------------------------------------------------------------
+
+// these are elements that will NOT change through the course of the game
+// back color of game tiles (grey), matching pairs...?
+// images: beachBall, snorkel, sun, palmTree, floats, sunscreen, sunglasses, sandCastle, sandals, popsicle, watermelon, pineapple, surfboard, swimsuit, umbrella
 
 let imageSet = [
     {name: 'beachBall1', image: 'tile_images/beachball.png', match: 'beachBall2', orderNum: 1},
@@ -33,21 +37,31 @@ let imageSet = [
     {name: 'umbrella2', image: 'tile_images/umbrella.png', match: 'umbrella1', orderNum: 30}
 ]
 
-const tilesFaceDown = {
-  0: 'grey'
-}
+// variables -------------------------------------------------------------------------------------------------------------------------------
 
-// variables ------------------------------------
+// these are elements that WILL change through the course of the game
+// score, timer, images of the tiles, location of the tiles (shuffle)
 
-let imageLocation
+// let score
+// begins at zero and increases with each matching pair the player chooses
+// only increases with matching pairs, not on every click
+// max number shown will be 15
+// when the score reaches 15, display winning message
+
+// let timer
+// starting from zero, when the first tile is clicked it begins timing the game play
+// continuously used through game
+// stops when the last pair of matching tiles is chosen
+
+// let tiles
+// location of tiles shuffles after every game, not on every turn
+// have an image associated with a tile number or something? 
 
 let tileClicked
 
 let imageMatched
 
-//let tileRandomOrder
-
-let imageFaceUp  //for flip..
+let imageFaceUp  
 
 let score
 
@@ -59,39 +73,50 @@ let button
 
 let firstClick
 
+// cached elements -------------------------------------------------------------------------------------------------------------------------
 
-
-// cached elements ------------------------------------
+// save necessary html elements as variables so we can refer to them later
+// button, message, tiles, score, timer
 
 const scoreEl = document.querySelector('#score')
-    //console.log('this is the score element', scoreEl)
 
 const messageEl = document.querySelector('#message')
-    //console.log('this is the message element', messageEl)
 
 const timerEl = document.querySelector('#timer')
-    //console.log('this is the timer element', timerEl)
 
 const playAgainButton = document.querySelector('button')
-    //console.log('this is the button element', playAgainButton)
 
 const board = [...document.querySelectorAll('#tiles > div')]
-   //console.log('this is the tile element', tileEls)
 
-// event listeners ------------------------------------
+// event listeners -------------------------------------------------------------------------------------------------------------------------
 
-//document.getElementById('tile2').addEventListener('click', handleClick)
+// what events happen when buttons and tiles are clicked, what they should be attached to, and what functions they call
+// handleClick and button
 
 document.getElementById('tiles').addEventListener('click', handleClick)
 
 playAgainButton.addEventListener('click', gameReset)
 
-// intialize ------------------------------------
+// functions -------------------------------------------------------------------------------------------------------------------------------
+
+// make things start to happen in the background with functions
+// initialize the game by setting values for variables
+// renders functions - board, scores, button, timer
+// render function
+// shuffle function
+// checking for a matching pair of tiles
+// handleClick function
+
+// initialize ------------------------------------------------------------------------------------------------------------------------------
+
+// sets variables to default values to get the game started
+// want tiles to be all face down& grey
+// want score and timer starting at 0
 
 init()
 
 function init() {
-  //imageLocation = []
+
   tileClicked = [
     false, false, false, false, false, false,
     false, false, false, false, false, false,
@@ -99,6 +124,7 @@ function init() {
     false, false, false, false, false, false,
     false, false, false, false, false, false
   ]
+  
   imageMatched = [
     false, false, false, false, false, false,
     false, false, false, false, false, false,
@@ -106,7 +132,7 @@ function init() {
     false, false, false, false, false, false,
     false, false, false, false, false, false
   ]
-  //tileRandomOrder = []
+
   imageFaceUp = [
     false, false, false, false, false, false,
     false, false, false, false, false, false,
@@ -114,22 +140,23 @@ function init() {
     false, false, false, false, false, false,
     false, false, false, false, false, false
   ]
+  
   score = 0
-  //timer = [0,0]
-  timer = 0
+
+  timer = '--'
+
   message = 'Click any tile to begin!!' 
+
   button = 'visible'
-
-//   firstClick = true
-
 
   render()
 
   firstClick = true
+
 }
 
 function gameReset() {
-    console.log('game reset function')
+
     tileClicked = [
         false, false, false, false, false, false,
         false, false, false, false, false, false,
@@ -137,6 +164,7 @@ function gameReset() {
         false, false, false, false, false, false,
         false, false, false, false, false, false
     ]
+    
     imageMatched = [
         false, false, false, false, false, false,
         false, false, false, false, false, false,
@@ -152,9 +180,13 @@ function gameReset() {
         false, false, false, false, false, false,
         false, false, false, false, false, false
     ]
+    
     score = 0
+    
     timer = 0
+    
     message = 'Click any tile to begin!!' 
+    
     button = 'visible'
     
     let = idx = 0
@@ -168,33 +200,52 @@ function gameReset() {
     firstClick = true
 }
 
-// render ------------------------------------
+// render -------------------------------------------------------------------------------------------------------------------------------
 
+// displays score on page
+// want to start at 0 and increase by 1 with each matching pair of images clicked
+// will not exceed 15
 function renderScore() {
     scoreEl.innerHTML = `SCORE: ` + score
 }
 
+// displays timer on page
+// want timer to start at 0 and begin timing when the first tile is clicked
+// want timer to stop when the last pair is matched (when the score = 15)
 function renderTimer() {
     timerEl.innerHTML = `TIMER: ` + timer
 }
 
+// displays message to player about game play
+// if player clicks on a matching pair of tiles, display "You got one!!"
+// if tiles clicked are not a match, display "Not a match. Try again!"
+// if all 15 matching pairs are clicked, display "YOU WIN!!!"
 function renderMessage() {
     messageEl.innerHTML = message
 }
 
+// displays play again button
+// want hidden until the score reached 15 (end of the game), then appear
+// when clicked, want to reset game and tiles turn face down and shuffle
+// want the timer and score to be reset back to 0
 function renderButton() {
     playAgainButton.style.visibility = (score === 15) ? 'visible' : 'hidden'
 }
 
+// displays all tiles face down until clicked
+// want tile to flip over to display the image when clicked, and stay on this side when another tile is clicked
+// if the two tiles are a match, tiles stay face up
+// if the two tiles are NOT a match, flip back to face down after 1 second
 function renderBoard() {
-    console.log('inside renderboard. first click: ', firstClick)
-    console.log('image face up array at TOP of render board', imageFaceUp)
+
     //first click section
     if (firstClick === true) {
-        console.log('inside first click section in render board')
+
         message = 'Choose another tile.'
         renderMessage()
+        
         let idx = 0
+        
         imageSet.forEach(function(image) {
 
             let images = document.createElement('img')
@@ -203,20 +254,21 @@ function renderBoard() {
             images.style.height = '18vmin'
             
             if (tileClicked[idx] === true) {
-                //console.log('bananas')
                 document.getElementById(`tile${idx}`).appendChild(images) 
                 tileClicked[idx] = false
             } 
+
             idx += 1
         })
+
         firstClick = false
-        
     }
 
     //second click section
     else {
-        console.log('inside second section in render board')
+
         idx = 0
+
         imageSet.forEach(function(image) {
 
             let images = document.createElement('img')
@@ -225,66 +277,34 @@ function renderBoard() {
             images.style.height = '18vmin'
             
             if (tileClicked[idx] === true) {
-                //console.log('bananas')
                 document.getElementById(`tile${idx}`).appendChild(images) 
                 tileClicked[idx] = false
             } 
+
             idx += 1
         })
+
         idx = 0
+
         setTimeout(function() {
             imageSet.forEach(function(image) {
                 if (imageFaceUp[idx] === true && imageMatched[idx] !== true) {
-                    console.log('inside render board match checker if statement')
                     document.getElementById(`tile${idx}`).innerHTML = '' 
                     imageFaceUp[idx] = false
                     message = 'Not a match. Try Again.'
                     renderMessage()
                 }
+
                 idx += 1
             })
 
             firstClick = true
-        }, 500)
+
+        }, 750)
     }
-    console.log('image face up array at BOTTOM of render board', imageFaceUp)
-
-//setTimeout(function() {
-            //     console.log('waited 1 sec') 
-            // }, 1000)
-
-    // let idx = 0
-    // console.log('renderBoard tileClicked array', tileClicked) // added 12:50
-    
-    // imageSet.forEach(function(image) {
-
-    //     let images = document.createElement('img')
-    //     images.src = imageSet[idx].image
-    //     images.style.width = '18vmin'
-    //     images.style.height = '18vmin'
-        
-    //     if (tileClicked[idx] === true) {
-    //         console.log('bananas')
-    //         document.getElementById(`tile${idx}`).appendChild(images) 
-    //         tileClicked[idx] = false
-    //     } 
-    //     else if (imageFaceUp[idx] === true && imageMatched[idx] === false) {
-    //         console.log('inside imageFaceUp[idx] else if. idx is', idx)
-    //         document.getElementById(`tile${idx}`).innerHTML = '' 
-    //     }
-
-    //     // if (imageMatched[idx] === false) {
-    //     //     console.log('inside renderBoard imageMatched checker. imageMatched: ', imageMatched)
-    //     //     document.getElementById(`tile${idx}`).innerHTML = '' 
-    //     // }
-
-        // idx += 1
-        
-    // })
-    //console.log('renderBoard imageFaceUp array after forEach', tileClicked) 
-    
 }
 
+//displays all elements with render functions attached (score, timer, tiles, button, message)
 function render() {
   renderScore()
   renderTimer()
@@ -293,33 +313,32 @@ function render() {
   renderBoard()
 }
 
-// other functions ------------------------------------
+// other functions -------------------------------------------------------------------------------------------------------------------------
+
+// when the player clicks on tiles or button
+// want tiles to flip over when clicked
+// when matching tiles display, want them to NOT be clickable
+// when the last pair is matched, want play again button to appear and be clickable - reseting game tiles, score, timer, and shuffling tiles
 
 function handleClick(event) {
     const idx = board.indexOf(event.target)
-    //let firstClick = true
-    console.log('index at click', idx)
+
     tileClicked[idx] = true
+
     imageFaceUp[idx] = true
-    console.log('handleclick imageFaceUp array', imageFaceUp)
-    //console.log('name of clicked tile', imageSet[idx].name)
 
     matchCheckerIdx = 0
-    //console.log('imageFaceUp array: ', imageFaceUp)
-    //console.log('ImageSet name: ', imageSet[idx].name)
-    imageSet.forEach(function(image) {
 
+    imageSet.forEach(function(image) {
         if (imageSet[idx].name === imageSet[matchCheckerIdx].match && imageFaceUp[matchCheckerIdx] === true) {
             score += 1
             message = 'You got a match!'
-            console.log('**** MATCH! **** matchcheckeridx for match is ', matchCheckerIdx)
             imageMatched[idx] = true
             imageMatched[matchCheckerIdx] = true
         } 
     
         matchCheckerIdx += 1
     })
-    console.log('IMAGE MATCHED ARRAY: ', imageMatched)
 
     if (score === 15) {
         message = 'YOU WIN!!!'
